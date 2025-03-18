70fe3d7f7bbb16cbb3b0ab96bca3beac356a2e91
xingeyang_imaging <singer.yang.yxg@gmail.com>
2025-03-18 12:47:21 +0300

    [Test] pass test, del test code

--- transform_json.py
+++
@@ -1,9 +0,0 @@
-
-from deeplens import GeoLens
-
-# main
-def main():
-    lens = GeoLens(filename=r"D:\cursor_deeplens\DeepLens-main\lenses\cellphone\cellphone68deg.json")
-    lens.write_lens_zmx(filename="./test.zmx")
-if __name__ == "__main__":
-    main()
