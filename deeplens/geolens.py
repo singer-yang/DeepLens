8917146ce098f3597b8dfb9f324e73f3a71308cb
xingeyang_imaging <singer.yang.yxg@gmail.com>
2025-04-24 19:10:02 +0300

    temp commit

--- deeplens/geolens.py
+++ deeplens/geolens.py
@@ -63,7 +63,7 @@ from .utils import (
     normalize_ImageNet,
     set_logger,
 )
-from .geolens_utils import draw_lens_layout, draw_layout_3d
+# from .geolens_utils import draw_lens_layout, draw_layout_3d
 
 
 class GeoLens(Lens):
@@ -2430,6 +2430,7 @@ class GeoLens(Lens):
         lens_title=None,
     ):
         """Plot lens layout with ray tracing."""
+        from deeplens.geolens_utils import draw_lens_layout
         draw_lens_layout(
             self,
             filename,
@@ -2452,6 +2453,7 @@ class GeoLens(Lens):
         Returns:
             fig, ax: Matplotlib figure and axis objects
         """
+        from deeplens.geolens_utils import draw_layout_3d
         return draw_layout_3d(
             self, filename=filename, figsize=figsize, view_angle=view_angle, show=show
         )
