0d851df2ed906d10cec98dc28229419c9fab7cf9
singeryang-autodl <singer.yang.yxg@gmail.com>
2025-09-03 21:45:38 +0800

    [fix] can draw lens 3d layout

--- visualization_demo.py
+++ visualization_demo.py
@@ -1,14 +1,13 @@
-from deeplens import GeoLens
-
 import os
+
+import matplotlib.pyplot as plt
 import numpy as np
 import torch
-import matplotlib.pyplot as plt
 
-import pyvista as pv
+from deeplens import GeoLens
+
 # import pyvistaqt as pvqt
-
-from deeplens.geolens_pkg.view_3d import draw_lens_3D
+from deeplens.geolens_pkg.view_3d import draw_lens_3d
 
 R = np.array([255, 0, 0])
 G = np.array([0, 255, 0])
@@ -23,19 +22,13 @@ lens = GeoLens(lens_config)
 
 (lens.draw_layout(os.path.join(SAVE_DIR, "lens_2dlayout.png")),)
 # plotter = pvqt.BackgroundPlotter()
-plotter = pv.Plotter(off_screen=True)
+# plotter = pv.Plotter(off_screen=True)
 
 hfov = lens.hfov
 
-draw_lens_3D(
-    plotter,
+draw_lens_3d(
     lens,
     fovs=[0.0, hfov * 0.99 * 57.296],
     fov_phis=[45.0, 135.0, 225.0, 315.0],
-    is_show_bridge=False,
     save_dir=SAVE_DIR,
 )
-
-# plotter.show()
-# plotter.app.exec_()
-plotter.screenshot(os.path.join(SAVE_DIR, "lens_3d_visualization.png"))
