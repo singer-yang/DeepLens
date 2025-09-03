5d1f9351909ec3915160fa773b8ed4b613c7ab47
singeryang-autodl <singer.yang.yxg@gmail.com>
2025-09-03 20:49:10 +0800

    [format] vis 3d

--- visualization_demo.py
+++ visualization_demo.py
@@ -10,30 +10,31 @@ import pyvista as pv
 
 from deeplens.geolens_pkg.view_3d import draw_lens_3D
 
-R = np.array([255,0,0])
-G = np.array([0,255,0])
-B = np.array([0,0,255])
+R = np.array([255, 0, 0])
+G = np.array([0, 255, 0])
+B = np.array([0, 0, 255])
 SAVE_DIR = "./visualization"
 if not os.path.exists(SAVE_DIR):
     os.mkdir(SAVE_DIR)
 
-lens_config = os.path.relpath(
-    "./lenses/cellphone/cellphone68deg.json"
-)
+lens_config = os.path.relpath("./lenses/cellphone/cellphone68deg.json")
 
 lens = GeoLens(lens_config)
 
-lens.draw_layout(os.path.join(SAVE_DIR, "lens_2dlayout.png")),
+(lens.draw_layout(os.path.join(SAVE_DIR, "lens_2dlayout.png")),)
 # plotter = pvqt.BackgroundPlotter()
 plotter = pv.Plotter(off_screen=True)
 
 hfov = lens.hfov
 
-draw_lens_3D(plotter,lens,
-             fovs= [0., hfov*0.99 * 57.296],
-             fov_phis = [45., 135., 225., 315.],
-             is_show_bridge=False,
-             save_dir=SAVE_DIR,)
+draw_lens_3D(
+    plotter,
+    lens,
+    fovs=[0.0, hfov * 0.99 * 57.296],
+    fov_phis=[45.0, 135.0, 225.0, 315.0],
+    is_show_bridge=False,
+    save_dir=SAVE_DIR,
+)
 
 # plotter.show()
 # plotter.app.exec_()
