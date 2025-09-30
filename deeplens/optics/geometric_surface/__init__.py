5b981dea2417129c8702b530c696e088c97c4089
xingeyang_imaging <singer.yang.yxg@gmail.com>
2025-09-30 17:26:53 +0300

    [new] prism class

--- deeplens/optics/geometric_surface/__init__.py
+++ deeplens/optics/geometric_surface/__init__.py
@@ -7,5 +7,6 @@ from .cubic import Cubic
 from .phase import Phase
 from .mirror import Mirror
 from .plane import Plane
+from .prism import Prism
 from .spheric import Spheric
 from .thinlens import ThinLens
